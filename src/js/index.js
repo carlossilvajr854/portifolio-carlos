fc078b4ccb95de3c1fc05168cc36c2d5c05bd5b7
Carlos Silva <carlossilvajr854@gmail.com>
2023-04-01 19:55:32 -0300

    Melhorias nos efeitos de menu e cabeçalho

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,31 @@
 const header = document.querySelectorAll("[data-header]");
 const divMother = document.querySelector(".mother");
 const lightbox = document.querySelector(".lightbox");
+const linksMenu = document.querySelectorAll(".link-menu");
+const linkActive = document.querySelector(".link-active");
+
+function removeClass(element, nameClass) {
+    element = document.querySelector("." + nameClass);
+    element.classList.remove(nameClass);
+}
+
+function addClass(element, nameClass) {
+    element.classList.add(nameClass);
+}
+
+function setCurrentPage() {
+    return Math.trunc(window.pageYOffset / window.innerHeight);
+}
+
+function setLinkActive(page) {
+    removeClass(linkActive, "link-active");
+    addClass(linksMenu[page], "link-active");
+}
 
 function controlSizeHeader() {
     let status = window.pageYOffset > 50 ? true : false;
+
+    setLinkActive(setCurrentPage());
 
     header[0].classList.toggle("animate-header", status);
     header[1].classList.toggle("animate-logo-header", status);
