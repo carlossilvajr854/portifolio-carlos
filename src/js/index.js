9e6dcebf4c98bbfbab0ad3085fa2515ea247d197
Carlos Silva <carlossilvajr854@gmail.com>
2023-03-31 23:49:41 -0300

    Home Finalizada

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,21 @@
+const header = document.querySelectorAll("[data-header]");
+const divMother = document.querySelector(".mother");
+const lightbox = document.querySelector(".lightbox");
+
 function controlSizeHeader() {
-    if (window.pageYOffset > 50) {
-        shrinkHeader();
-    } else {
-        increaseHeader();
-    }
-}
+    let status = window.pageYOffset > 50 ? true : false;
 
-function shrinkHeader() {
-    let tagHeader = document.getElementsByTagName("header")[0];
-    tagHeader.style.animation = "";
-    tagHeader.classList.add("headerMinimizado");
-    let logo = document.getElementsByClassName("logo")[0];
-    logo.style.animation = "";
-    logo.classList.add("logoMinimizada");
-    let hamburger = document.getElementsByClassName("hamburger")[0];
-    hamburger.style.animation = "";
-    hamburger.classList.add("hamburgerMinimizado");
-}
-
-function increaseHeader() {
-    let tagHeader = document.getElementsByTagName("header")[0];
-    tagHeader.style.animation = "aumentarHeader 1s";
-    tagHeader.classList.remove("headerMinimizado");
-    let logo = document.getElementsByClassName("logo")[0];
-    logo.style.animation = "aumentarLogo 1s";
-    logo.classList.remove("logoMinimizada");
-    let hamburger = document.getElementsByClassName("hamburger")[0];
-    hamburger.style.animation = "aumentarHamburger 1s";
-    hamburger.classList.remove("hamburgerMinimizado");
+    header[0].classList.toggle("animate-header", status);
+    header[1].classList.toggle("animate-logo-header", status);
+    header[2].classList.toggle("animate-hamburger-header", status);
 }
 
 function showMenu() {
-    let divMother = document.querySelector(".mother");
-    divMother.classList.toggle("show-menu");
+    divMother.classList.toggle("show-menu", true);
+    lightbox.classList.toggle("active-lightbox", true);
+}
+
+function hideMenu() {
+    divMother.classList.toggle("show-menu", false);
+    lightbox.classList.toggle("active-lightbox", false);
 }
