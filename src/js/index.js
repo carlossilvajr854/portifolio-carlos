a2f61d35ec332e76004baa4c0952d43a9a11b699
Carlos <carlossilvajr854@gmail.com>
2023-05-10 19:26:43 -0300

    Finalização da página Sobre mim e Início da página Habilidades

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,29 @@
 const NAVBAR = document.querySelector(".navbar");
+const LINKS_MENU = document.querySelectorAll(".nav-link");
+const LINK_ATIVO = document.querySelector(".nav-link .active");
 
-function controlSizeHeader() {
+function removeClasse(elemento, nomeClasse) {
+    elemento = document.querySelector("." + nomeClasse);
+    elemento.classList.remove(nomeClasse);
+}
+
+function adicionaClasse(elemento, nomeClasse) {
+    elemento.classList.add(nomeClasse);
+}
+
+function definePaginaAtual() {
+    return Math.trunc(window.pageYOffset / window.innerHeight);
+}
+
+function marcaLinkAtivo(pagina) {
+    removeClasse(LINK_ATIVO, "active");
+    adicionaClasse(LINKS_MENU[pagina], "active");
+}
+
+function ajustaNavbar() {
     let status = null;
+
+    marcaLinkAtivo(definePaginaAtual());
 
     status = window.pageYOffset > 50 ? true : false;
     NAVBAR.classList.toggle("navbar-retract", status);
